0d30f438d0638e2f64640ec6f5eaa701f9c94612
Albi <alimanialbi@gmail.com>
2025-09-13 13:42:33 +0200

    M

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        int x = 10;
+        int y = 20;
+
+        if (x + y == 30) {
+            System.out.println("Claculations are exact");
+        } else {
+            System.out.println("Calculations are not corret, Please check the math");
+        }
+
+
+
+
     }
 }
