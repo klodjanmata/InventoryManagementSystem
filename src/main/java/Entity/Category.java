5c492659ad5a9d712a45b883a9c7845e4a892083
Albi <alimanialbi@gmail.com>
2025-09-17 21:41:06 +0200

    M

--- src/main/java/Entity/Category.java
+++ src/main/java/Entity/Category.java
@@ -5,13 +5,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "Category")
 @AllArgsConstructor
 @NoArgsConstructor
-
+@Getter
+@Setter
 public class Category {
     @Id
     @GeneratedValue
